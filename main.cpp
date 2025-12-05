13008b1a1ef27d313d7b5d120af3110052b708e6
Neatink <minyaylo51@gmail.com>
2025-12-06 01:21:07 +0200

    Add table for game;output table

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+char table[3][3][3] = {
+	"#","#","#",
+	"#","#","#",
+	"#","#","#",
+};
+
 int main() {
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			for (int p = 0; p < 3; p++) {
+				cout << table[i][j][p];
+			}
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
