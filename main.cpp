4edddcb2fd42d140096fa78f831e4677a6c906ed
Neatink <minyaylo51@gmail.com>
2025-12-06 20:47:32 +0200

    Move out table to function;Improve function createPlayer

--- main.cpp
+++ main.cpp
@@ -13,30 +13,34 @@ struct Player{
 	char symbol;
 };
 
-void createPlayer(struct Player *player, const int number) {
+void createPlayer(struct Player &player, const int number) {
 	cout << "Player" << number << " name: ";
-	cin >> player->name;
+	cin >> player.name;
 	cout << "Player" << number << " symbol: ";
-	cin >> player->symbol;
+	cin >> player.symbol;
 
 	cout << "Player" << number << " created!\n";
 }
 
-int main() {
-	Player Player1;
-	Player Player2;
-
-	createPlayer(&Player1, 1);
-	createPlayer(&Player2, 2);
-
-	cout << "Go play!\n";
-
+void outGameTable(char (&table)[3][3]) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			cout << table[i][j] << " ";
 		}
 		cout << endl;
 	}
+}
+
+int main() {
+	Player Player1;
+	Player Player2;
+
+	createPlayer(Player1, 1);
+	createPlayer(Player2, 2);
+
+	cout << "Go play!\n";
+
+	outGameTable(table);
 
 	return 0;
 }
