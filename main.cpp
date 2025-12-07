1b5b0219fc65c5acf1c6e39d5c61e64ee360b399
Neatink <minyaylo51@gmail.com>
2025-12-07 02:26:46 +0200

    Now player can move

--- main.cpp
+++ main.cpp
@@ -9,17 +9,19 @@ char table[3][3] = {
 };
 
 struct Player{
+	uint16_t number;
 	string name;
 	char symbol;
 };
 
-void createPlayer(struct Player &player, const int number) {
-	cout << "Player" << number << " name: ";
+void createPlayer(Player &player, const uint16_t number) {
+	player.number = number;
+	cout << "Player " << number << " name: ";
 	cin >> player.name;
-	cout << "Player" << number << " symbol: ";
+	cout << "Player " << number << " symbol: ";
 	cin >> player.symbol;
 
-	cout << "Player" << number << " created!\n";
+	cout << "Player " << number << " created!\n";
 }
 
 void outGameTable(char (&table)[3][3]) {
@@ -28,6 +30,21 @@ void outGameTable(char (&table)[3][3]) {
 			cout << table[i][j] << " ";
 		}
 		cout << endl;
+	}
+}
+
+void playerMove(Player &player) {
+	uint16_t column, row, sum;
+	cout << "Player " << player.number << ", enter column and row(example: 1 3): ";
+	cin >> column >> row;
+	sum = column + row;
+	if ((sum >= 2) && (sum <= 6)) {
+		table[column - 1][row - 1] = player.symbol;
+		outGameTable(table);
+	}
+	else {
+		cout << "Incorrect column or row!\nTry again.\n";
+		return;
 	}
 }
 
@@ -41,6 +58,10 @@ int main() {
 	cout << "Go play!\n";
 
 	outGameTable(table);
+	
+	while (true) {
+		playerMove(Player1);
+	}
 
 	return 0;
 }
